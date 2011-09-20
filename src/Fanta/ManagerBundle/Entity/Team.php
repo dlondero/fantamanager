e5d4cd947031ecf860969c31a71511a61655fcc1
Daniel Londero <daniel.londero@gmail.com>
2011-09-20 10:32:00 +0200

    Added Team entity.

---
+++ src/Fanta/ManagerBundle/Entity/Team.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Fanta\ManagerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Fanta\ManagerBundle\Entity\Team
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Fanta\ManagerBundle\Entity\TeamRepository")
+ */
+class Team
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function __toString()
+    {
+        return $this->name;
+    }
+}
